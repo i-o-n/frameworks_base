6fe923bfbad0d9e9466dbcb2274bfa8348af52d7
Lokesh Chamane <lokesh.c703@gmail.com>
2016-04-09 19:54:40 -0400

    base: HeadsUp snooze function
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Forward ported to marshmallow/Nougat By:@BeansTown106
    *pretty much a rewrite using the logic from my heads up timeout port
    *also added a second toast for 1 minute instead of saying 1 minutes
    included Make Heads Up snooze toast more informative by beanstown106 too
    *tell the user which app is snoozing
    *also center the toast to account for big app names
    *RU
    Example: SystemUI - Peeking notifications are snoozing for 1 minute.
    
    Forward ported to 10 by: @eyosen
    
    Change-Id: Ia6d0ab44eb49ba4f70a345150f7c40d64105e691

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5371,6 +5371,15 @@ public final class Settings {
         /** @hide */
         private static final Validator NOTIFICATION_GUTS_KILL_APP_BUTTON_VALIDATOR = BOOLEAN_VALIDATOR;
 
+       /**
+        * Defines the global heads up notification snooze
+        * @hide
+        */
+        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
+
+        /** @hide */
+        private static final Validator HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR = new SettingsValidators.InclusiveIntegerRangeValidator(0, 1200000);
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -5512,6 +5521,7 @@ public final class Settings {
             STATUS_BAR_QUICK_QS_PULLDOWN,
             STATUS_BAR_BRIGHTNESS_CONTROL,
             NOTIFICATION_GUTS_KILL_APP_BUTTON,
+            HEADS_UP_NOTIFICATION_SNOOZE,
         };
 
         /**
@@ -5714,6 +5724,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BATTERY_LEVEL_CHARGE_ALARM_ENABLED);
             PRIVATE_SETTINGS.add(SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND);
             PRIVATE_SETTINGS.add(NOTIFICATION_GUTS_KILL_APP_BUTTON);
+            PRIVATE_SETTINGS.add(HEADS_UP_NOTIFICATION_SNOOZE);
         }
 
         /**
@@ -5891,6 +5902,7 @@ public final class Settings {
             VALIDATORS.put(BATTERY_LEVEL_CHARGE_ALARM_ENABLED, BATTERY_LEVEL_CHARGE_ALARM_ENABLED_VALIDATOR);
             VALIDATORS.put(SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND, SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_GUTS_KILL_APP_BUTTON, NOTIFICATION_GUTS_KILL_APP_BUTTON_VALIDATOR);
+            VALIDATORS.put(HEADS_UP_NOTIFICATION_SNOOZE,HEADS_UP_NOTIFICATION_SNOOZE_VALIDATOR);
         }
 
         /**
