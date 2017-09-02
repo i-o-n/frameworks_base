61ee9e5cd17141ff3b74374524f63d8e29202e6f
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    WakelockBlocker [1/2] ported by @faust93

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5487,6 +5487,18 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+         /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
