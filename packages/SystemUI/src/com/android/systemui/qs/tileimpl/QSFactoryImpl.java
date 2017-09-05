9d39d209799000813f284c5fb48432071c7caa62
Decad3nce <decad3nce@gmail.com>
2017-09-06 00:37:11 +0800

    SystemUI:Add LTE Tile @xyyx: reworked with slash

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
@@ -138,6 +139,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ScreenStabilizationTile(mHost);
             case "reading_mode":
                 return new ReadingModeTile(mHost);
+            case "lte":
+                return new LteTile(mHost);
         }
 
         // Intent tiles.
