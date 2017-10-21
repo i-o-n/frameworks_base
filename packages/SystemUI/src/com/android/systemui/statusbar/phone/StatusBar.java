7fe1e97e40db646d9c356c245e055bfbc84bc757
ezio84 <brabus84@gmail.com>
2017-10-21 14:46:24 +0200

    Slim Recents 3.0 [1/3]
    Refactored/improved everything.
    Slim Recents won't never die.
    http://i.imgur.com/PO3RSeg.gif
    
    [SpiritCroc]
    Bring to P
    
    Change-Id: I9deb0412e2421df2ac2755f2111a3cec70a7d2c4

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -48,8 +48,10 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.AlarmManager;
+import android.app.IActivityManager;
 import android.app.IWallpaperManager;
 import android.app.KeyguardManager;
 import android.app.Notification;
@@ -202,6 +204,7 @@ import com.android.systemui.recents.events.activity.UndockingTaskEvent;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.settings.BrightnessController;
 import com.android.systemui.shared.system.WindowManagerWrapper;
+import com.android.systemui.slimrecent.RecentController;
 import com.android.systemui.stackdivider.Divider;
 import com.android.systemui.stackdivider.WindowManagerProxy;
 import com.android.systemui.statusbar.ActivatableNotificationView;
@@ -1620,6 +1623,18 @@ public class StatusBar extends SystemUI implements DemoMode,
             final int navbarPos = WindowManagerWrapper.getInstance().getNavBarPosition();
             if (navbarPos == NAV_BAR_POS_INVALID) {
                 return false;
+            }
+            if (mSlimRecents != null) {
+                boolean isInLockTaskMode = false;
+                try {
+                    IActivityManager activityManager = ActivityManagerNative.getDefault();
+                    if (activityManager.isInLockTaskMode()) {
+                        isInLockTaskMode = true;
+                   }
+                } catch (RemoteException e) {}
+                if (!isInLockTaskMode) {
+                    return mSlimRecents.startMultiWindow();
+                }
             }
             int createMode = navbarPos == NAV_BAR_POS_LEFT
                     ? ActivityManager.SPLIT_SCREEN_CREATE_MODE_BOTTOM_OR_RIGHT
@@ -3702,6 +3717,10 @@ private void swapWhiteBlackAccent() {
 
         mViewHierarchyManager.updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
+
+        if (mSlimRecents != null) {
+            mSlimRecents.onConfigurationChanged(newConfig);
+        }
     }
 
     @Override
@@ -5298,6 +5317,9 @@ private void swapWhiteBlackAccent() {
             resolver.registerContentObserver(Settings.System.getUriFor(
                    Settings.System.BATTERY_ESTIMATE_POSITION),
                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                  Settings.System.USE_SLIM_RECENTS),
+                  false, this, UserHandle.USER_ALL);
 	    }
 
         @Override
@@ -5327,6 +5349,9 @@ private void swapWhiteBlackAccent() {
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.BATTERY_ESTIMATE_POSITION))) {
                 restartUI();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.USE_SLIM_RECENTS))) {
+                updateRecentsMode();
             }
         }
 
@@ -5339,6 +5364,7 @@ private void swapWhiteBlackAccent() {
             updateLockscreenFilter();
             updateTelephonyIcons();
             updateKeyguardStatusSettings();
+            updateRecentsMode();
         }
     }
 
@@ -5879,6 +5905,8 @@ private void swapWhiteBlackAccent() {
     protected Display mDisplay;
 
     protected RecentsComponent mRecents;
+
+    protected RecentController mSlimRecents;
 
     protected NotificationShelf mNotificationShelf;
     protected FooterView mFooterView;
@@ -6637,4 +6665,29 @@ private void swapWhiteBlackAccent() {
                     saveImportance.run();
                 }
             };
+
+    private void updateRecentsMode() {
+        boolean slimRecents = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 0, UserHandle.USER_CURRENT) == 1;
+        if (slimRecents) {
+            mRecents.evictAllCaches();
+            mRecents.removeSbCallbacks();
+            mSlimRecents = new RecentController(mContext);
+            rebuildRecentsScreen();
+            mSlimRecents.addSbCallbacks();
+        } else {
+            mRecents.addSbCallbacks();
+            if (mSlimRecents != null) {
+                mSlimRecents.evictAllCaches();
+                mSlimRecents.removeSbCallbacks();
+                mSlimRecents = null;
+            }
+        }
+    }
+
+    private void rebuildRecentsScreen() {
+        if (mSlimRecents != null) {
+            mSlimRecents.rebuildRecentsScreen();
+        }
+    }
 }
