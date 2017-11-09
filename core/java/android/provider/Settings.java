9dbcba182ea5da013d6cba32c54e28c60e5202e1
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-11-09 17:31:07 +0530

    Screen Stabiization [1/3]
    Forward port to Pie by @SuperDroidBond
    
    Change-Id: Ibccb8d78d79938f42241ea3690ea66c3aace89ea

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -120,6 +120,16 @@ public final class Settings {
     public static final String ACTION_SETTINGS = "android.settings.SETTINGS";
 
     /**
+     * Activity Action: Show screen satbilization settings.
+     * <p>
+     * Input: Nothing.
+     * <p>
+     * Output: Nothing.
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String SCREEN_STABILIZATION_SETTINGS = "android.settings.SCREEN_STABILIZATION";
+    
+    /**
      * Activity Action: Show settings to allow configuration of APNs.
      * <p>
      * Input: Nothing.
@@ -5050,6 +5060,19 @@ public final class Settings {
          */
         public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
 
+        /** 
+         * Screen stabilization parameters
+         * @hide
+         */
+        public static final String STABILIZATION_ENABLE = "stabilization_enable";
+        
+        public static final String STABILIZATION_VELOCITY_FRICTION = "stabilization_velocity_friction";
+        
+        public static final String STABILIZATION_POSITION_FRICTION = "stabilization_position_friction";
+        
+        public static final String STABILIZATION_LOWPASS_ALPHA = "stabilization_lowpass_alpha";
+        
+        public static final String STABILIZATION_VELOCITY_AMPLITUDE = "stabilization_velocity_amplitude";
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
