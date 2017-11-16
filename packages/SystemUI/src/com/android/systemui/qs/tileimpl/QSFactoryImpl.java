ddb67ee21777f3e76d80540fc5deb353e3daff59
Altaf-Mahdi <altaf.mahdi@gmail.com>
2017-11-15 22:36:51 -0500

    QS: Add Screenshot QS Tile
    alpabatize tile list
    
    Change-Id: I9961ed353a9539a582a5fa3fdc4ffbaa10bab031
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -103,6 +104,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "sync":
                 return new SyncTile(mHost);
+            case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
