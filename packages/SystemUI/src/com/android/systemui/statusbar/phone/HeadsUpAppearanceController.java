485d28a73e329b98b3324e6f6496b53052208389
Tetsutoki Shiozawa <tetsutoki.shiozawa@sony.com>
2018-09-05 13:17:01 +0900

    SystemUI: Fix overlap between operator name and heads-up
    The title of heads-up notification was shown incompletely
    becaue it's covered by operator name.
    To fix this issue, the operator name should be hidden
    when a heads-up notification is shown.
    
    Fixes:
    Test: atest SystemUITests
    Test: manual - enable config_showOperatorNameInStatusBar
                   and show a heads-up notification
    
    Change-Id: Id3318cd7cb2c9b6950392fadd11d02ab873d84c5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/HeadsUpAppearanceController.java
@@ -52,6 +52,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
     private final NotificationStackScrollLayout mStackScroller;
     private final HeadsUpStatusBarView mHeadsUpStatusBarView;
     private final View mClockView;
+    private final View mOperatorNameView;
     private final DarkIconDispatcher mDarkIconDispatcher;
     private final NotificationPanelView mPanelView;
     private final Consumer<ExpandableNotificationRow>
@@ -76,7 +77,8 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                 statusbarView.findViewById(R.id.heads_up_status_bar_view),
                 statusbarView.findViewById(R.id.notification_stack_scroller),
                 statusbarView.findViewById(R.id.notification_panel),
-                statusbarView.findViewById(R.id.clock));
+                statusbarView.findViewById(R.id.clock),
+                statusbarView.findViewById(R.id.operator_name_frame));
     }
 
     @VisibleForTesting
@@ -86,7 +88,8 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
             HeadsUpStatusBarView headsUpStatusBarView,
             NotificationStackScrollLayout stackScroller,
             NotificationPanelView panelView,
-            View clockView) {
+            View clockView,
+            View operatorNameView) {
         mNotificationIconAreaController = notificationIconAreaController;
         mHeadsUpManager = headsUpManager;
         mHeadsUpManager.addListener(this);
@@ -102,6 +105,7 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
         mStackScroller.addOnLayoutChangeListener(mStackScrollLayoutChangeListener);
         mStackScroller.setHeadsUpAppearanceController(this);
         mClockView = clockView;
+        mOperatorNameView = operatorNameView;
         mDarkIconDispatcher = Dependency.get(DarkIconDispatcher.class);
         mDarkIconDispatcher.addDarkReceiver(this);
     }
@@ -223,12 +227,20 @@ public class HeadsUpAppearanceController implements OnHeadsUpChangedListener,
                         CONTENT_FADE_DELAY /* delay */);
                 CrossFadeHelper.fadeOut(mClockView, CONTENT_FADE_DURATION/* duration */,
                         0 /* delay */, () -> mClockView.setVisibility(View.INVISIBLE));
+                if (mOperatorNameView != null) {
+                    CrossFadeHelper.fadeOut(mClockView, CONTENT_FADE_DURATION/* duration */,
+                            0 /* delay */, () -> mOperatorNameView.setVisibility(View.INVISIBLE));
+                }
             } else {
                 if (clockStyle == 0) {
                     CrossFadeHelper.fadeIn(mClockView, CONTENT_FADE_DURATION /* duration */,
                             CONTENT_FADE_DELAY /* delay */);
                 } else {
                     mClockView.setVisibility(View.GONE);
+                }
+                if (mOperatorNameView != null) {
+                    CrossFadeHelper.fadeIn(mOperatorNameView, CONTENT_FADE_DURATION /* duration */,
+                            CONTENT_FADE_DELAY /* delay */);
                 }
                 CrossFadeHelper.fadeOut(mHeadsUpStatusBarView, CONTENT_FADE_DURATION/* duration */,
                         0 /* delay */, () -> mHeadsUpStatusBarView.setVisibility(View.GONE));
