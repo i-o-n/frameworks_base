55aedbfd59d4ec7cbb6f272ee4326a0e140d7342
maxwen <max.weninger@gmail.com>
2018-10-06 12:37:43 +0200

    base: SystemUI: add tuner to hide qs security footer
    and fix footer bg color to match the rest of the qs panel
    
    Change-Id: If80b2aeb1484a42fbf39f3b011f1d7c5388d1be0

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -76,6 +76,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     public static final String QS_SHOW_HEADER = "qs_show_header";
     public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
+    public static final String QS_SHOW_SECURITY = "qs_show_secure";
 
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
@@ -181,6 +182,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mSettingObserver.update();
         final TunerService tunerService = Dependency.get(TunerService.class);
         tunerService.addTunable(this, QS_BRIGHTNESS_POSITION_BOTTOM);
+        tunerService.addTunable(this, QS_SHOW_SECURITY);
 
         if (mHost != null) {
             setTiles(mHost.getTiles());
@@ -223,6 +225,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                 addView(mBrightnessView, getBrightnessViewPositionBottom());
                 mBrightnessBottom = true;
             }
+        }
+        if (QS_SHOW_SECURITY.equals(key)) {
+            mFooter.setForceHide(newValue != null && Integer.parseInt(newValue) == 0);
         }
     }
 
