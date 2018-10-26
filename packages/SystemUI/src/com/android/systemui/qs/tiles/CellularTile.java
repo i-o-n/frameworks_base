343df631c6d9db5a9d4444f63162793b6008ae33
Ali B <abittin@gmail.com>
2018-10-27 01:49:38 +0300

    QS: make Cellular tile DualTarget
    Change-Id: I798233da06ada24a3501f32fe00412fb807f750d

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -79,6 +79,11 @@ public class CellularTile extends QSTileImpl<SignalState> {
     }
 
     @Override
+    public boolean isDualTarget() {
+        return true;
+    }
+
+    @Override
     public SignalState newTileState() {
         return new SignalState();
     }
