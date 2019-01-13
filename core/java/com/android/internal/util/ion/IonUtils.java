b0015c5e6890867319945b6dfcf8f41b33947f2e
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1

--- core/java/com/android/internal/util/ion/IonUtils.java
+++ core/java/com/android/internal/util/ion/IonUtils.java
@@ -42,6 +42,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.net.ConnectivityManager;
 import android.os.Looper;
+import android.util.DisplayMetrics;
 import android.view.InputDevice;
 import android.view.IWindowManager;
 import android.view.KeyCharacterMap;
@@ -361,5 +362,24 @@ public class IonUtils {
                 (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo network = (connManager != null) ? connManager.getActiveNetworkInfo() : null;
         return network != null;
+    }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
