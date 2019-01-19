af207cff41638c5123b625407dd7b80ac7ac982c
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    IonUtils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/com/android/internal/util/ion/IonUtils.java
+++ core/java/com/android/internal/util/ion/IonUtils.java
@@ -22,6 +22,7 @@ import android.app.NotificationManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -32,6 +33,7 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.BatteryManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
@@ -80,6 +82,19 @@ public class IonUtils {
      * @hide
      */
     public static final String DISMISS_KEYGUARD_EXTRA_INTENT = "launch";
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
 
     // Clear notifications
     public static void clearAllNotifications() {
