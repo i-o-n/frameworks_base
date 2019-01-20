758a83bd448503e892bed0ca48c0ef133e69a407
AnkitGourav <Ankitgourav035@gmail.com>
2019-01-20 20:59:29 +0530

    fix SleepScreenTile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SleepScreenTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SleepScreenTile.java
@@ -88,6 +88,6 @@ public class SleepScreenTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.HAVOC_SETTINGS;
+        return MetricsEvent.ION_IONIZER;
     }
 }
