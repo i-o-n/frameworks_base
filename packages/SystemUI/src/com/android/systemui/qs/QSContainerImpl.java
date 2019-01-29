820e1abb92e53f86c71f16c53db3ba8a9ceeff5a
maxwen <max.weninger@gmail.com>
2019-01-29 14:10:34 +0100

    base: SystemUI: header image tweaks aka the big marginator [1/2]
    Change-Id: I10475bcfa783c5ba645749990b7374e2254b8956

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -63,7 +63,6 @@ public class QSContainerImpl extends FrameLayout implements
     private int mSideMargins;
     private boolean mQsDisabled;
 
-    private Drawable mQsBackGround;
     private int mQsBackGroundAlpha;
 
     private boolean mHeaderImageEnabled;
@@ -92,8 +91,8 @@ public class QSContainerImpl extends FrameLayout implements
         mStatusBarBackground = findViewById(R.id.quick_settings_status_bar_background);
         mBackgroundGradient = findViewById(R.id.quick_settings_gradient_view);
         mSideMargins = getResources().getDimensionPixelSize(R.dimen.notification_side_paddings);
-        mQsBackGround = getContext().getDrawable(R.drawable.qs_background_primary);
         mBackgroundImage = findViewById(R.id.qs_header_image_view);
+        mBackgroundImage.setClipToOutline(true);
         updateSettings();
         updateResources();
 
@@ -233,16 +232,26 @@ public class QSContainerImpl extends FrameLayout implements
                 com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
                 mContext.getResources().getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
-        int statusBarSideMargin = mContext.getResources().getDimensionPixelSize(
-                R.dimen.qs_header_image_side_margin);
+        int statusBarSideMargin = mHeaderImageEnabled ? mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_header_image_side_margin) : 0;
 
-        ((LayoutParams) mQSPanel.getLayoutParams()).topMargin = topMargin;
+        // always add the margin below the statusbar with or without image
+        int statusBarBottomMargin = mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_header_image_bottom_margin);
+
+        ((LayoutParams) mQSPanel.getLayoutParams()).topMargin = topMargin + statusBarBottomMargin;
         mQSPanel.setLayoutParams(mQSPanel.getLayoutParams());
 
         ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mStatusBarBackground.getLayoutParams();
         lp.height = topMargin;
         lp.setMargins(statusBarSideMargin, 0, statusBarSideMargin, 0);
         mStatusBarBackground.setLayoutParams(lp);
+
+        if (mHeaderImageEnabled) {
+            mStatusBarBackground.setBackgroundColor(Color.TRANSPARENT);
+        } else {
+            mStatusBarBackground.setBackgroundColor(Color.BLACK);
+        }
     }
 
     /**
