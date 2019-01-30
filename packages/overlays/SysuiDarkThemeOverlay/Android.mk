4126deeca23e28decd9849546442ba450761d069
AnkitGourav <Ankitgourav035@gmail.com>
2019-01-30 14:40:38 +0530

    Disable SysuiDarkThemeOverlay

--- packages/overlays/SysuiDarkThemeOverlay/Android.mk
+++ packages/overlays/SysuiDarkThemeOverlay/Android.mk
@@ -1,14 +1,14 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH:= $(call my-dir)
+#include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := SysuiDarkTheme
-LOCAL_CERTIFICATE := platform
+#LOCAL_PACKAGE_NAME := SysuiDarkTheme
+#LOCAL_CERTIFICATE := platform
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
+#LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+#LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := SysuiDarkThemeOverlay
-LOCAL_SDK_VERSION := current
+#LOCAL_PACKAGE_NAME := SysuiDarkThemeOverlay
+#LOCAL_SDK_VERSION := current
 
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
