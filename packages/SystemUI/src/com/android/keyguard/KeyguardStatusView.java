ceaac70b13741a8d41ab0e1b809eb4b113b59b64
ZeNiXxX <vitjahermann@gmail.com>
2019-02-02 18:52:59 +0530

    Revert "Auto switch to a dark analog clock when using dark system theme"
    This reverts commit acbb1f00291701feca5b1568031958e153d91a7b.

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -215,10 +215,6 @@ public class KeyguardStatusView extends GridLayout implements
 
     }
 
-    public void onThemeChanged(boolean useDarkTheme) {
-        mCustomClockView.onThemeChanged(useDarkTheme, true);
-    }
-
     /**
      * Moves clock and separator, adjusting margins when slice content changes.
      */
