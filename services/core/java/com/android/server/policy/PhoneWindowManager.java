505f17cda0d49f9ee6dc68250f0b4739cf6f4e82
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-02 17:05:57 -0300

    Fix screen pinning unlock with hwkeys
    Missed on https://github.com/PixelExperience/frameworks_base/commit/58a3ca2f64e250714c563dd0f1477104506e6d07#diff-8f544f361f7e05df0a4abe2f4fa97908
    
    Change-Id: Ie7e4347b96c6db99f7d4788d76619ec9c93d23df

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -161,6 +161,7 @@ import android.annotation.Nullable;
 import android.app.ActivityManager;
 import android.app.ActivityManagerInternal;
 import android.app.ActivityManagerInternal.SleepToken;
+import android.app.ActivityManagerNative;
 import android.app.ActivityThread;
 import android.app.AppOpsManager;
 import android.app.IUiModeManager;
@@ -1930,6 +1931,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private void backLongPress() {
         mBackKeyHandled = true;
+
+        if (unpinActivity()) {
+            return;
+        }
 
         switch (mLongPressOnBackBehavior) {
             case LONG_PRESS_BACK_NOTHING:
@@ -4890,6 +4895,20 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
             } catch (Exception e) {
                 Slog.w(TAG, "Could not dispatch event to device key handler", e);
+            }
+        }
+        return false;
+    }
+
+    private boolean unpinActivity() {
+        if (!hasNavigationBar()) {
+            try {
+                if (ActivityManagerNative.getDefault().isInLockTaskMode()) {
+                    ActivityManagerNative.getDefault().stopSystemLockTaskMode();
+                    return true;
+                }
+            } catch (RemoteException e) {
+                // ignore
             }
         }
         return false;
