fe1ebf2b80fce6835ddc0317b27d4b6fa282e49d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-30 12:21:17 -0300

    fwb: Drop keydisabler
    Change-Id: Ibd206cbf7f5fb9c67d3a6841bc5a849568b086c4

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -307,8 +307,6 @@ import com.android.server.wm.DisplayFrames;
 import com.android.server.wm.WindowManagerInternal;
 import com.android.server.wm.WindowManagerInternal.AppTransitionListener;
 
-import org.pixelexperience.keydisabler.KeyDisabler;
-
 import com.android.internal.util.custom.hwkeys.ActionUtils;
 import static com.android.internal.util.custom.hwkeys.DeviceKeysConstants.*;
 
@@ -3095,15 +3093,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             updateRotation(true);
         }
     }
-    
+
     private void updateKeydisabler(){
-        try {
-            if (KeyDisabler.isSupported()) {
-                KeyDisabler.setActive(mHasNavigationBar);
-            }
-        } catch (Exception e) {
-            Slog.w(TAG, "KeyDisabler operation failed", e);
-        }
+        // TODO: Add hwkeys code here
     }
 
     private void updateWakeGestureListenerLp() {
