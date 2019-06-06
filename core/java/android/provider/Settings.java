beba147e489a9e8d9590b1ee587b7fdea17a3417
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable charging animation [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5634,6 +5634,12 @@ public final class Settings {
         public static final String POWER_MENU_BG_STYLE = "power_menu_bg_style";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
