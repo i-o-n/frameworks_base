81a361e18724d5f6d3c5ed5c605801c7faa3f3ab
DennySPB <dennyspb@gmail.com>
2019-06-07 17:32:51 +0300

    GamingMode: Rewrite implementation [1/2]
    - move to GamingModeController
    - remove useless QS tile
    - depends only on running package names
    - track top-app activity change for enabling/disabling gaming mode
    - dynamic mode adds determined game into game list
    - make notification/toasts optional
    - update game list if package was uninstalled
    - remove volume maximize option (total mess if using bt headphones)
    - remove mess with dialer on AOD
    
    Change-Id: I4eeda8c96e068160400786e1f0f2451e69f8024c
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -35,7 +35,6 @@ import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
-import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -138,8 +137,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new LiveDisplayTile(mHost);
             case "reading_mode":
                 return new ReadingModeTile(mHost);
-            case "gaming":
-                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
