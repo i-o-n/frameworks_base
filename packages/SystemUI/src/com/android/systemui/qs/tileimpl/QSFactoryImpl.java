d8100e8dabcf06078e5033821040d5d46e521975
Pranav Vashi <neobuddy89@gmail.com>
2019-07-06 20:40:36 +0530

    base: Add Gaming mode tile
    * Also clean up leftover code.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I5e1b719fd550608a55a3e2dd80b6401e2c287011

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -36,6 +36,7 @@ import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
+import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -144,6 +145,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new LteTile(mHost);
             case "reboot":
                 return new RebootTile(mHost);
+            case "gaming":
+                return new GamingModeTile(mHost);
         }
 
         // Intent tiles.
