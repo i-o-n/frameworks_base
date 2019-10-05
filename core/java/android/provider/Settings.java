b0eba298756d51e6cda2fd0c2a2dcf37104841fd
AnkitGourav <Ankitgourav035@gmail.com>
2019-10-05 09:37:47 +0530

    Unlock Lockscreen with biometric after reboot [1/2]
    original commit https://github.com/i-o-n/frameworks_base/commit/b058c2daba1e0ed25b55d5784777ecc7471dcacd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4925,6 +4925,12 @@ public final class Settings {
         private static final Validator SHOW_CPU_OVERLAY_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
