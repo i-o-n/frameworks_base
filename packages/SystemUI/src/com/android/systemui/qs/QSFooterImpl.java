AOSPA/android_frameworks_base-old

SystemUI: Show something usefull in the QS footer
 * Show #StayParanoid instead of that useless build version.

Change-Id: I012b70d5b1ad247f7ee34b545c043a6d4d556475

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -166,10 +166,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         TextView v = findViewById(R.id.build);
         if (v == null) return;
         if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
+            v.setText("#StayParanoid");
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);

i-o-n/frameworks_base

 * Show #ion instead of that useless build version.
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -177,10 +177,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
         TextView v = findViewById(R.id.build);
         if (v == null) return;
         if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
+            v.setText("#ion");
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);