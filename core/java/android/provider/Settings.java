ca88e1fd1f9feee7a007047089ae257ffa44ef23
ezio84 <brabus84@gmail.com>
2019-10-27 21:51:01 +0100

    ScreenRec: show MediaProjection permission warning only once
    it's really annoying to have to press OK in that warning
    popup each time we start recording
    
    The popup was called by MediaProjectionManage createScreenCaptureIntent

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5319,6 +5319,12 @@ public final class Settings {
         private static final Validator STATUS_BAR_QUICK_QS_PULLDOWN_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * Whether the user has already accepted MediaProjection permission for the built-in screenrecorder
+         * @hide
+         */
+        public static final String MEDIAPROJECTION_SYSUI_OK = "mediaprojection_sysui_ok";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
