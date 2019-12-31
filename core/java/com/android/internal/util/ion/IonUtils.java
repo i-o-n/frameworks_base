ac06c22fd2de57f7e6e4bccb5c2a1415127a03c4
Nicolas Dhouailly <ndhouailly@gmail.com>
2019-12-31 21:01:50 +0100

    IonUtils: Add a method to enable/disable package components
    Change-Id: If4b176409323f9b919a65e10d9fc41236f2c5894
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/com/android/internal/util/ion/IonUtils.java
+++ core/java/com/android/internal/util/ion/IonUtils.java
@@ -17,6 +17,7 @@
 package com.android.internal.util.ion;
 
 import android.Manifest;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -244,6 +245,15 @@ public class IonUtils {
                         InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
             }
         }, 20);
+    }
+
+    public static void setComponentState(Context context, String packageName,
+            String componentClassName, boolean enabled) {
+        PackageManager pm  = context.getApplicationContext().getPackageManager();
+        ComponentName componentName = new ComponentName(packageName, componentClassName);
+        int state = enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        pm.setComponentEnabledSetting(componentName, state, PackageManager.DONT_KILL_APP);
     }
 
     public static void setPartialScreenshot(boolean active) {
