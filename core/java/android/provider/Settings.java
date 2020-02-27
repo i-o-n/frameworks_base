60d8bd4e51d4eb1290ae0ad3b30deb4802a2ae2d
Joey Huab <joey@evolution-x.org>
2020-02-27 13:21:33 +0000

    SystemUI: Add OP Ripple animations [1/2]
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6442,7 +6442,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_ANIM_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 17);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 18);
 
         /**
          * Whether to change the color of the qs panel
