08e3e6bcf4f2012c1263ea7ca81f19e3780864ef
El Dainosor <eldainosor@gmail.com>
2020-04-06 21:51:43 +0000

    Screenrec: Add support for AudioPlaybackCapture
    Using the new API to record internal audio, and using the MediaMuxer workaround
    because of google only allowing AudioRecord as audio output.
    
    Also, full credits to @kcrimi for the MediaMuxer code.
    Taken from recorder-activity from:
    https://github.com/kcrimi/recorder-activity
    
    Bootleggers (@ElDainosor) edit: Adapted to AOSP screen recorder code. Added stereo support and pause recording
    (as the normal MediaRecorder process has pause support)
    
    Co-authored by: kcrimi <kevincrimi@gmail.com>
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6041,10 +6041,10 @@ public final class Settings {
         private static final Validator DOZE_BRIGHTNESS_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
-         * Screenrecord: enable mic
+         * Screenrecord: audio source
          * @hide
          */
-        public static final String SCREENRECORD_ENABLE_MIC = "screenrecord_enable_mic";
+        public static final String SCREENRECORD_AUDIO_SOURCE = "screenrecord_audio_source";
 
         /**
          * Screenrecord: show taps
