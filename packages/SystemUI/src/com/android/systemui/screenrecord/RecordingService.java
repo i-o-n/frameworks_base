2285e1fe9047674c824f028e3bfef65a9e577d8a
AnkitGourav <Ankitgourav035@gmail.com>
2020-04-30 05:17:58 +0530

    Revert "Screenrecord: Correctly display notification title"
    This reverts commit ed38c5876ea7bae4f49f15fb35b81454292cb53d.
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -36,7 +36,6 @@ import android.media.MediaRecorder;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Handler;
 import android.os.Looper;
@@ -334,14 +333,11 @@ public class RecordingService extends Service {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(channel);
 
-        final Bundle extras = new Bundle();
-        extras.putString(Notification.EXTRA_SUBSTITUTE_APP_NAME, this.getString(R.string.global_action_screenrecord));
         mRecordingNotificationBuilder = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_screenrecord)
                 .setContentTitle(getResources().getString(R.string.screenrecord_name))
                 .setUsesChronometer(true)
-                .setOngoing(true)
-                .addExtras(extras);
+                .setOngoing(true);
         setNotificationActions(false, notificationManager);
         Notification notification = mRecordingNotificationBuilder.build();
         startForeground(NOTIFICATION_ID, notification);
@@ -399,8 +395,6 @@ public class RecordingService extends Service {
                         PendingIntent.FLAG_UPDATE_CURRENT))
                 .build();
 
-        final Bundle extras = new Bundle();
-        extras.putString(Notification.EXTRA_SUBSTITUTE_APP_NAME, this.getString(R.string.global_action_screenrecord));
         Notification.Builder builder = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_screenrecord)
                 .setContentTitle(getResources().getString(R.string.screenrecord_name))
@@ -412,8 +406,7 @@ public class RecordingService extends Service {
                         Intent.FLAG_GRANT_READ_URI_PERMISSION))
                 .addAction(shareAction)
                 .addAction(deleteAction)
-                .setAutoCancel(true)
-                .addExtras(extras);
+                .setAutoCancel(true);
 
         // Add thumbnail if available
         Bitmap thumbnailBitmap = null;
