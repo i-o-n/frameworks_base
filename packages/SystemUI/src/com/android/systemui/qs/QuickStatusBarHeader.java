DerpLab/platform_frameworks_base

QSB: Only apply header padding if a custom header image is selected

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -167,6 +167,12 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER), false,
                     this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER_IMAGE), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.OMNI_STATUS_BAR_FILE_HEADER_IMAGE), false,
+                    this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -180,6 +186,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     // omni additions start
     private boolean mLandscape;
     private boolean mHeaderImageEnabled;
+    private String mCustomHeaderImage;
+    private String mCustomHeaderFile;
 
     private class OmniSettingsObserver extends ContentObserver {
         OmniSettingsObserver(Handler handler) {
@@ -380,9 +388,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         /*mHeaderTextContainerView.getLayoutParams().height =
                 resources.getDimensionPixelSize(R.dimen.qs_header_tooltip_height);
         mHeaderTextContainerView.setLayoutParams(mHeaderTextContainerView.getLayoutParams());*/
+        boolean headerImageSelected = mHeaderImageEnabled &&
+                (mCustomHeaderImage != null || mCustomHeaderFile != null);
 
         int topMargin = resources.getDimensionPixelSize(
-                com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
+                com.android.internal.R.dimen.quick_qs_offset_height) + (headerImageSelected ?
                 resources.getDimensionPixelSize(R.dimen.qs_header_image_offset) : 0);
 
         mSystemIconsView.getLayoutParams().height = topMargin;
@@ -395,7 +405,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             int qsHeight = resources.getDimensionPixelSize(
                     com.android.internal.R.dimen.quick_qs_total_height);
 
-            if (mHeaderImageEnabled) {
+            if (headerImageSelected) {
                 qsHeight += resources.getDimensionPixelSize(R.dimen.qs_header_image_offset);
             }
 
@@ -653,6 +663,12 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mCustomHeaderImage = Settings.System.getStringForUser(getContext().getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+        mCustomHeaderFile = Settings.System.getStringForUser(getContext().getContentResolver(),
+                Settings.System.OMNI_STATUS_BAR_FILE_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
         isShowDragHandle = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.QS_DRAG_HANDLE, 1,
                 UserHandle.USER_CURRENT) == 1;

i-o-n/frameworks_base

Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -145,6 +145,8 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
     private boolean mHeaderImageEnabled;
     private int mHeaderImageHeight;
+    private String mCustomHeaderImage;
+    private String mCustomHeaderFile;
 
     // System Info
     private TextView mSystemInfoText;
@@ -204,6 +206,12 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                     this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.QS_SYSTEM_INFO), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE), false,
+                    this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_FILE_HEADER_IMAGE), false,
                     this, UserHandle.USER_ALL);
             }
 
@@ -515,9 +523,11 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mHeaderTextContainerView.getLayoutParams().height =
                 resources.getDimensionPixelSize(R.dimen.qs_header_tooltip_height);
         mHeaderTextContainerView.setLayoutParams(mHeaderTextContainerView.getLayoutParams());
+        boolean headerImageSelected = mHeaderImageEnabled &&
+                (mCustomHeaderImage != null || mCustomHeaderFile != null);
 
         int topMargin = resources.getDimensionPixelSize(
-                com.android.internal.R.dimen.quick_qs_offset_height) + (mHeaderImageEnabled ?
+                com.android.internal.R.dimen.quick_qs_offset_height) + (headerImageSelected ?
                 mHeaderImageHeight : 0);
 
         mSystemIconsView.getLayoutParams().height = topMargin;
@@ -530,7 +540,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
             int qsHeight = resources.getDimensionPixelSize(
                     com.android.internal.R.dimen.quick_qs_total_height);
 
-            if (mHeaderImageEnabled) {
+            if (headerImageSelected) {
                 qsHeight += mHeaderImageHeight;
             }
 
@@ -905,6 +915,12 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mHeaderImageEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
                 UserHandle.USER_CURRENT) == 1;
+        mCustomHeaderImage = Settings.System.getStringForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
+        mCustomHeaderFile = Settings.System.getStringForUser(getContext().getContentResolver(),
+                Settings.System.STATUS_BAR_FILE_HEADER_IMAGE,
+                UserHandle.USER_CURRENT);
         int mImageHeight = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER_HEIGHT, 25,
                 UserHandle.USER_CURRENT);