6b83547f4c2e75377dbf354bc7777667043046ed
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-03 11:21:23 +0530

    Revert "Remove AOSP Accents [1/3]"
    This reverts commit cf97c6ad5f0ccb8a7b01ad92a6f24d64fdf86547.
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -17,6 +17,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := frameworks-base-overlays
 LOCAL_REQUIRED_MODULES := \
+	AccentColorBlackOverlay \
+	AccentColorCinnamonOverlay \
+	AccentColorOceanOverlay \
+	AccentColorOrchidOverlay \
+	AccentColorSpaceOverlay \
+	AccentColorGreenOverlay \
+	AccentColorPurpleOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
 	DisplayCutoutEmulationTallOverlay \
