Havoc-OS/android_frameworks_base

Gestures nav: Add Skip/Previous Song Function [1/2]
abcdu: use our statusbar media actions implementation to have also remote spotify support,
and to be able to send different media actions from any class with the proper keycode

Change-Id: I9ef673b9c684c3754d4a0e99340de3023367f6b0

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -692,6 +692,12 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 13: // Kill app
                 ActionUtils.killForegroundApp();
                 break;
+            case 14: // Skip song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                break;
         }
     }
 

i-o-n/frameworks_base

Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -686,6 +686,12 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 13: // Kill app
                 IonUtils.killForegroundApp();
                 break;
+            case 14: // Skip song
+                IonUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                IonUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                break;
         }
     }
 