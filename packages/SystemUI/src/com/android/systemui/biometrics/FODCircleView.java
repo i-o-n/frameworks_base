1b61b6e51f7c83f414140a18fb1ea7fa65e592b5
ritik-gupta <ritikg8126@gmail.com>
2020-05-24 17:54:50 +0000

    Revert "FODCircleView: invert default icon path and add #20000000 bg"
    This reverts commit b6788fa57ff38ff97daba39764955593d67df47b.
    
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -67,9 +67,6 @@ public class FODCircleView extends ImageView {
 
     private int mDreamingOffsetY;
 
-    private int mColor;
-    private int mColorBackground;
-
     private boolean mIsBouncer;
     private boolean mIsDreaming;
     private boolean mIsPulsing;
@@ -194,8 +191,6 @@ public class FODCircleView extends ImageView {
     public FODCircleView(Context context) {
         super(context);
 
-        setScaleType(ScaleType.CENTER);
-
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
         if (daemon == null) {
             throw new RuntimeException("Unable to get IFingerprintInscreen");
@@ -212,11 +207,8 @@ public class FODCircleView extends ImageView {
 
         Resources res = context.getResources();
 
-        mColor = res.getColor(R.color.config_fodColor);
-        mColorBackground = res.getColor(R.color.config_fodColorBackground);
-
         mPaintFingerprint.setAntiAlias(true);
-        mPaintFingerprint.setColor(mColorBackground);
+        mPaintFingerprint.setColor(res.getColor(R.color.config_fodColor));
 
         mWindowManager = context.getSystemService(WindowManager.class);
 
@@ -252,8 +244,11 @@ public class FODCircleView extends ImageView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
         super.onDraw(canvas);
+
+        if (mIsCircleShowing) {
+            canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+        }
     }
 
     @Override
@@ -349,7 +344,6 @@ public class FODCircleView extends ImageView {
         updateAlpha();
         dispatchPress();
 
-        mPaintFingerprint.setColor(mColor);
         setImageResource(R.drawable.fod_icon_pressed);
         invalidate();
     }
