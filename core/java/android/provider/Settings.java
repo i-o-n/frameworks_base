0c57178065e220bb81575ae62655c3a28e5d01c7
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-08 21:35:58 +0300

    Make sensor block package list configurable [1/3]
    Based on 0603892bf4638f6904011491963ad941c8ed306e
    
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7200,6 +7200,13 @@ public final class Settings {
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
+         * Sensor blocked packages
+         * @hide
+         */
+        public static final String SENSOR_BLOCKED_APP = "sensor_blocked_app";
+        public static final String SENSOR_BLOCKED_APP_DUMMY = "sensor_blocked_app_dummy";
+
+        /**
          * Whether to show only navbar handle
          * @hide
          */
